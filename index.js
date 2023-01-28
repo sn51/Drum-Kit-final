78d53614388ad18b8acbf363e2293fb541236cce
sn51 <snehjhaa0612@gmail.com>
2023-01-29 00:21:49 +0530

    animation added

--- index.js
+++ index.js
@@ -6,11 +6,16 @@ var audio_dict ={w:'sounds/crash.mp3',
                 ,d:'sounds/tom-1.mp3',j:'sounds/tom-2.mp3',k:'sounds/tom-3.mp3'
                 ,l:'sounds/tom-4.mp3'};
 
+
+function makeSound(event){
+   
+    var audio = new Audio(event);
+    audio.play();
+}
 for(var i=0;i<arr.length;i++){
     arr[i].addEventListener("click",function(){
-       
-       var audio = new Audio(audio_dict[(this.innerHTML)]);
-       audio.play();
+       makeSound(audio_dict[this.innerHTML]);
+       makeAnimation(this.innerHTML);
     });
 }
 
@@ -20,11 +25,18 @@ document.addEventListener("keydown",function(e){
     var key_arr = Object.keys(audio_dict); //getting the keys array from our previous dictionary
     //checking if key pressed is present in key array
     if(key_arr.includes(key_pressed)){
-        //then only playing sound
-        var audio = new Audio(audio_dict[key_pressed]);
-        audio.play();
+        makeSound(audio_dict[key_pressed]);
+        makeAnimation(key_pressed);
     }
     
 });
+
+function makeAnimation(currentKey){
+    var activeButton = document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },200);
+}
 
   
